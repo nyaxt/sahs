d2109ed0ba1de0b98c75c5a461bf609416e3799c
nyaxt <ueno@nyaxtstep.com>
2012-08-13 09:58:27 +0900

    add comments

--- sahs.hs
+++ sahs.hs
@@ -8,36 +8,42 @@ import System.Environment( getArgs )
 type Index = Int
 type IntAry = AU.UArray Int Int
 type SA = IntAry
-type PhiA = IntAry
+type PsiA = IntAry
 
-tailsI_ :: Int -> String -> [(Int, String)]
-tailsI_ off "" = [(off, "")]
-tailsI_ off xxs@(_:xs) = (off, xxs) : tailsI_ (off+1) xs
-
+-- str to list of (offset, suffix)
 tailsI :: String -> [(Int, String)]
 tailsI str = tailsI_ 0 str
+  where tailsI_ :: Int -> String -> [(Int, String)]
+	tailsI_ off "" = [(off, "")]
+	tailsI_ off xxs@(_:xs) = (off, xxs) : tailsI_ (off+1) xs
 
-genSAAry :: String -> [Int]
-genSAAry str = map fst $ sortBy (\a b -> compare (snd a) (snd b)) $ tailsI str
+-- construct suffix array in list
+genSAList :: String -> [Int]
+genSAList str = map fst $ sortBy (\a b -> compare (snd a) (snd b)) $ tailsI str
 
+-- construct suffix array
 genSA :: String -> SA
-genSA src = AU.listArray (0, length src) (genSAAry src)
+genSA src = AU.listArray (0, length src) (genSAList src)
 
+-- a[b] -> b[a]
 invArray :: IntAry -> IntAry
 invArray a = AU.array (AU.bounds a) (map swap $ AU.assocs a)
 
+-- return length of IntAry. assumes that IntAry start w/ index 0
 arrayLen :: IntAry -> Int
 arrayLen a = 1 + (snd $ AU.bounds a)
 
+-- given function f(i) which produces value for index i, generate IntAry length n
 iterateAry :: (Index -> Int) -> Int -> IntAry
 iterateAry f n = AU.array (0, n-1) $ (flip map) [0..n-1] $ \i -> (i, f i)
 
-genPhiA :: SA -> PhiA
-genPhiA sa =
+-- generate \Psi array from SA in naive way
+genPsiA :: SA -> PsiA
+genPsiA sa =
   let n = arrayLen sa
       isa = invArray sa
   in (flip iterateAry) n $ \i -> isa AU.! (((sa AU.! i) + 1) `mod` n)
 
--- main :: IO ()
+main :: IO ()
 -- main = getArgs >>= (print . show . genSA . head)
-main = putStrLn $ show $ AU.elems $ genPhiA $ genSA "abracadabra"
+main = putStrLn $ show $ AU.elems $ genPsiA $ genSA "abracadabra"
